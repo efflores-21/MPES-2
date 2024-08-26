a5502d6cc8ba578b7fc22f557f457524f556c8e7
efflores-21 <efflores@uamv.edu.ni>
2024-08-25 22:06:44 -0600

    Clase 25/8/2024

---
+++ Sesion#3/Sesion#3/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion_3
+{
+    internal class Persona
+
+    {
+        public Persona() { }
+        public String Nombre { get; set; }
+        public DateTime FechaNac { get; set; }
+        public int CalcularEdad()
+        {
+            int edad = 0;
+            try
+            {
+                edad = DateTime.Now.Year - FechaNac.Year;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error{ ex.Message}");
+            }
+            return edad;
+        }
+        public string EvaluarEdad()
+        {
+            if (CalcularEdad() >= 18) return "Mayor de Edad";
+            return "Menor de Edad";
+        }
+    }
+}
---
+++ Sesion#3/Sesion#3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sesion_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            string nombre;
+            DateTime fechaNac;
+            Console.WriteLine("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Fecha de Nacimiento: ");
+            fechaNac = DateTime.Parse(Console.ReadLine());
+            persona.Nombre = nombre;
+            persona.FechaNac = fechaNac;
+            Console.WriteLine(persona.EvaluarEdad());
+        }
+    }
+}
